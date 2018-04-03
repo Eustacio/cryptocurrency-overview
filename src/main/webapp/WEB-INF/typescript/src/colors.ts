1195bb3c1a57e8dd3b8ee8851790621f5fed4819
Eustácio <wallison.gth@gmail.com>
2018-04-03 17:59:42 -0300

    Added a new TypeScript module to export color values that are necessary in another TypeScript files

---
+++ src/main/webapp/WEB-INF/typescript/src/colors.ts
@@ -0,0 +1,7 @@
+export module ChartColor {
+  // Color for the chart when the coin price is raising
+  export const priceRaising = '#2CC02C';
+
+  // Color for the chart when the coin price is dropping
+  export const priceDropping = '#F73030';
+}
