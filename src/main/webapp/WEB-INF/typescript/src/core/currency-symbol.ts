3e3f341fbdd773944accdd72b7f4657d6dff4d78
Eustácio <wallison.gth@gmail.com>
2018-04-04 17:38:09 -0300

    Added the CurrencySymbol TypeScript enum to store the commonly used currencies acronyms along with its corresponding symbol

---
+++ src/main/webapp/WEB-INF/typescript/src/core/currency-symbol.ts
@@ -0,0 +1,17 @@
+// TODO: add documentation
+export enum CurrencySymbol {
+  BTC = 'Ƀ',
+  LTC = 'Ł',
+  DAO = 'Ð',
+  USD = '$',
+  CNY = '¥',
+  EUR = '€',
+  GBP = '£',
+  JPY = '¥',
+  PLN = 'zł',
+  RUB = '₽',
+  ETH = 'Ξ',
+  GOLD = 'Gold g',
+  INR = '₹',
+  BRL = 'R$'
+}
