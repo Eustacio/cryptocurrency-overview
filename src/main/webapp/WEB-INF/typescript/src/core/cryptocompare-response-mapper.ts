be671995d48bf28b84e054de5faedc91cde18774
Eustácio <wallison.gth@gmail.com>
2018-04-04 17:43:05 -0300

    Added the CryptoCompareResponseMapper#getCurrencySymbol method, to map the currency acronym to the correspondent symbol

--- src/main/webapp/WEB-INF/typescript/src/core/cryptocompare-response-mapper.ts
+++ src/main/webapp/WEB-INF/typescript/src/core/cryptocompare-response-mapper.ts
@@ -1,4 +1,10 @@
+import { CurrencySymbol } from './currency-symbol';
+
 // TODO: add documentation
 export class CryptoCompareResponseMapper {
+
+  getCurrencySymbol(symbol: string): CurrencySymbol | string {
+    return CurrencySymbol[symbol] || symbol;
+  }
 
 }
