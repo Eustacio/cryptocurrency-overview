71f590473b4c8559315ccf821684f9e24bfdab79
Eustácio <wallison.gth@gmail.com>
2018-04-04 18:11:34 -0300

    Added the CryptoCompareResponseType TypeScript enum, to store the message types that can be returned from the CryptoCompare websocket

---
+++ src/main/webapp/WEB-INF/typescript/src/core/cryptocompare-response-type.ts
@@ -0,0 +1,20 @@
+// TODO= add documentation
+export enum CryptoCompareResponseType {
+  TRADE = '0',
+  FEEDNEWS = '1',
+  CURRENT = '2',
+  LOADCOMPLATE = '3',
+  COINPAIRS = '4',
+  CURRENTAGG = '5',
+  TOPLIST = '6',
+  TOPLISTCHANGE = '7',
+  ORDERBOOK = '8',
+  FULLORDERBOOK = '9',
+  ACTIVATION = '10',
+
+  TRADECATCHUP = '100',
+  NEWSCATCHUP = '101',
+
+  TRADECATCHUPCOMPLETE = '300',
+  NEWSCATCHUPCOMPLETE = '301',
+}
