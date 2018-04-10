5e1bfeb839b1811b850bc1786a8a361dd40672f3
Eustácio <wallison.gth@gmail.com>
2018-04-10 18:42:20 -0300

    Added the method "getResponseType" to the TypeScript class CryptoCompareResponseMapper , to extract the response type from the CryptoCompare websocket API

--- src/main/webapp/WEB-INF/typescript/src/core/cryptocompare-response-mapper.ts
+++ src/main/webapp/WEB-INF/typescript/src/core/cryptocompare-response-mapper.ts
@@ -1,10 +1,21 @@
 import { CurrencySymbol } from '../domain/currency-symbol';
+import { CryptoCompareResponseType } from '../domain/cryptocompare-response-type';
 
 // TODO: add documentation
 export class CryptoCompareResponseMapper {
 
   getCurrencySymbol(symbol: string): CurrencySymbol | string {
     return CurrencySymbol[symbol] || symbol;
+  }
+
+  getResponseType(response: string): CryptoCompareResponseType | undefined {
+    const responseType: string = response.substring(0, response.indexOf('~'));
+
+    for (const key in CryptoCompareResponseType) {
+      if (CryptoCompareResponseType[key] === responseType) {
+        return <CryptoCompareResponseType>key;
+      }
+    }
   }
 
 }
