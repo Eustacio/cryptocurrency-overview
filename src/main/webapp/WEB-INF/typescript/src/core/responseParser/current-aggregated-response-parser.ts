c3c3a44b77736e56ea1b72bffe45414cd871aed6
Eustácio <wallison.gth@gmail.com>
2018-04-11 13:53:43 -0300

    Added the method "extractDataFromResponse" to the TypeScript class CurrentAggregatedResponseParser, to convert the response from the CryptoCompare API into a object

--- src/main/webapp/WEB-INF/typescript/src/core/responseParser/current-aggregated-response-parser.ts
+++ src/main/webapp/WEB-INF/typescript/src/core/responseParser/current-aggregated-response-parser.ts
@@ -1,9 +1,34 @@
 import { AbstractResponseParser } from './AbstractResponseParser';
+import { Current } from '../../domain/data-type/current';
 
 export class CurrentAggregatedResponseParser extends AbstractResponseParser {
 
   parse(response: string): any {
 
+  }
+
+  private extractDataFromResponse(response: string): any {
+    const responseValues: string[] = this.responseMapper.splitResponse(response);
+    const mask: number = this.getResponseMask(responseValues);
+
+    let data = {};
+    let currentField = 0;
+
+    Object.keys(Current).forEach(key => {
+      if (Current[key] === 0) {
+        data[key] = responseValues[currentField];
+        currentField++;
+      } else if (mask & Current[key]) {
+        if (key === 'LASTMARKET') {
+          data[key] = responseValues[currentField];
+        } else {
+          data[key] = parseFloat(responseValues[currentField]);
+        }
+        currentField++;
+      }
+    });
+
+    return data;
   }
 
 }
