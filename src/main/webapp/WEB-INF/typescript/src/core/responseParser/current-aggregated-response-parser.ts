2d379b45327e8812c88cd20c41e7484a3c4b599c
Eustácio <wallison.gth@gmail.com>
2018-04-12 18:47:29 -0300

    Added the method "normalizeResponse" to the CurrentAggregatedResponseParser TypeScript class, to group the response data into a object

--- src/main/webapp/WEB-INF/typescript/src/core/responseParser/current-aggregated-response-parser.ts
+++ src/main/webapp/WEB-INF/typescript/src/core/responseParser/current-aggregated-response-parser.ts
@@ -32,6 +32,38 @@ export class CurrentAggregatedResponseParser extends AbstractResponseParser {
     return data;
   }
 
+  private normalizeResponse(data: any): any {
+    let normalizedResponse: any = {};
+
+    if (data) {
+      // The FROMSYMBOL and TOSYMBOL keys are replaced by the 'symbols' key.
+      // The BID, OFFER and AVG fields are not returned in the response.
+
+      normalizedResponse['response_type'] = data['TYPE'];
+      normalizedResponse['market'] = data['MARKET'];
+      normalizedResponse['symbols'] = this.extractCurrencySymbols(data);
+      normalizedResponse['flag'] = data['FLAGS'];
+      normalizedResponse['price'] = data['PRICE'];
+      normalizedResponse['last_update'] = data['LASTUPDATE'];
+      normalizedResponse['last_volume'] = data['LASTVOLUME'];
+      normalizedResponse['last_volume_to'] = data['LASTVOLUMETO'];
+      normalizedResponse['last_trade_id'] = data['LASTTRADEID'];
+      normalizedResponse['volume_hour'] = data['VOLUMEHOUR'];
+      normalizedResponse['volume_hour_to'] = data['VOLUMEHOURTO'];
+      normalizedResponse['volume_24_hour'] = data['VOLUME24HOUR'];
+      normalizedResponse['volume_24_hour_to'] = data['VOLUME24HOURTO'];
+      normalizedResponse['open_hour'] = data['OPENHOUR'];
+      normalizedResponse['high_hour'] = data['HIGHHOUR'];
+      normalizedResponse['low_hour'] = data['LOWHOUR'];
+      normalizedResponse['open_24_hour'] = data['OPEN24HOUR'];
+      normalizedResponse['high_24_hour'] = data['HIGH24HOUR'];
+      normalizedResponse['low_24_hour'] = data['LOW24HOUR'];
+      normalizedResponse['last_market'] = data['LASTMARKET'];
+    }
+
+    return normalizedResponse;
+  }
+
   private extractCurrencySymbols(responseData: any): ConvertedCurrencySymbol {
     const currency: string = responseData['TOSYMBOL'];
     const cryptocurrency: string = responseData['FROMSYMBOL'];
