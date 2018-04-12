07a44b3a0d9a49548279123fa092bd44a0603017
Eustácio <wallison.gth@gmail.com>
2018-04-12 18:33:51 -0300

    Changed the "getResponseType" method from the CryptoCompareResponseMapper TypeScript class, to return the response type only as a string value

--- src/main/webapp/WEB-INF/typescript/src/core/cryptocompare-response-mapper.ts
+++ src/main/webapp/WEB-INF/typescript/src/core/cryptocompare-response-mapper.ts
@@ -1,5 +1,4 @@
 import { CurrencySymbol } from '../domain/currency-symbol';
-import { CryptoCompareResponseType } from '../domain/cryptocompare-response-type';
 
 // TODO: add documentation
 export class CryptoCompareResponseMapper {
@@ -12,16 +11,8 @@ export class CryptoCompareResponseMapper {
     return response.split('~');
   }
 
-  getResponseType(response: string): CryptoCompareResponseType | string {
-    const responseType: string = response.substring(0, response.indexOf('~'));
-
-    for (const key in CryptoCompareResponseType) {
-      if (CryptoCompareResponseType[key] === responseType) {
-        return <CryptoCompareResponseType>key;
-      }
-    }
-
-    return responseType;
+  getResponseType(response: string): string {
+    return response.substring(0, response.indexOf('~'));
   }
 
 }
