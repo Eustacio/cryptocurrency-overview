47d002fb1a963d12b46df491e1efe609619d6478
Eustácio <wallison.gth@gmail.com>
2018-04-13 13:21:57 -0300

    Added the method "buildSubscription" to the CryptoCompareService TypeScript class, to create an CryptoCompare websocket API subscription string for each cryptocurrency requested

--- src/main/webapp/WEB-INF/typescript/src/service/cryptocompare-service.ts
+++ src/main/webapp/WEB-INF/typescript/src/service/cryptocompare-service.ts
@@ -1,3 +1,13 @@
+import { ExchangeMarket } from '../domain/exchange-market';
+import { CryptoCompareMarketDataType } from '../domain/cryptocompare-market-data-type';
+
 export class CryptoCompareService {
+
+  private buildSubscription(marketData: CryptoCompareMarketDataType, exchangeMarket: ExchangeMarket,
+                            coinAcronym: string[], currency: string): string[] {
+    return coinAcronym.map(acronym =>
+      `${marketData}~${exchangeMarket}~${acronym}~${currency}`
+    );
+  }
 
 }
